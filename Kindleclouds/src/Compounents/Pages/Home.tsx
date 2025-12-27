0e9fa9722c2cbff4e2e9711f299935a8564dfc66
vamshikrishnap478 <vk480606@gmail.com>
2025-12-28 02:38:26 +0530

    new project

---
+++ Kindleclouds/src/Compounents/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import react from "react"
+import header from "../Header"
+
+const home : react.FC  = () => {
+    return (
+        <><header></header>
+        <div><h1>Comming Soon</h1></div>
+        </>
+    )
+}
+export default home
